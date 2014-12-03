de75873f9a7ee8c3380df09567ce63e70c4184be
Sergio Merida <sergiotabarini@hotmail.com>
2014-12-03 00:21:31 -0600

    v3

--- js/script.js
+++ js/script.js
@@ -1,77 +1,92 @@
+busqueda = [];
+url = [];
+filters = [];
+
 $(document).ready(function(){
     $('#butt').click(function(){
-        var b = $('input:text[name=nombre]').val();
-        var service_url = 'https://www.googleapis.com/freebase/v1/search';
-        var params = {
-            'query': b,
-            'limit': 5,
-            'indent': true
-        };
+	var b = $('input:text[name=nombre]').val();
+	var filter1 = '(all type:/music/artist)';
+	var filter2 = '(all type:/people/person)';
+	var filter3 = '(all type:/book/)';
+ 	var service_url = 'https://www.googleapis.com/freebase/v1/search';
+	var params = {
+        	'query': b,
+        	'limit': 50,
+        	'indent': true
+    	};
+	busqueda.push(b);
+	url.push(service_url);
+	filters.push(filter1);
+	filters.push(filter2);
+	filters.push(filter3);
+	console.log(filters);
         $.getJSON(service_url + '?callback=?', params, function(response) {
             $.each(response.result, function(i, result) {
                 var obj = {text: result['name']}.text;
-                var insertar = "<tr>" + "<td>" + obj + "</td>" + "</tr>";
+                var notable = {text: result['notable']['name']}.text;                
+		var insertar = "<tr class='search'>"+ "<td>"+ (i+1) +"</td>" + "<td>" + obj + "</td>"+ "<td>"+notable+"</td>" + "</tr>";
                 $('.mostrar').append(insertar);
             });/*--each--*/
         }); /*--getjson--*/
     }); /*--click--*/
 	
     $(".clear").click(function() {
-        $(".mostrar").empty();
+        $(".search").empty();
     }); /*--Clear--*/
+
 }); /*--document--*/
 
-
-/*----------------------First Filter-----------------------------*/
-   $(document).ready(function(){
-        $('input[name="foo"]').click(function(){
-            if($(this).prop("checked") == true){
-                var b = $('input:text[name=nombre]').val();
-        	var service_url = 'https://www.googleapis.com/freebase/v1/search';
-        	var params = {
-            	'query': b,
-            	'filter': '(all type:/music/)',
-            	'limit': 5,
-            	'indent': true
-        	};
-        $.getJSON(service_url + '?callback=?', params, function(response) {
+/*------------------*/
+$(document).ready(function(){
+$('input[name="foo"]').click(function(){
+	$('.search').hide();
+        if($(this).prop("checked") == true){
+            var params = {
+        	'query': busqueda[0],
+		'filter': filters[0],
+        	'limit': 5,
+        	'indent': true
+    		};
+	    $.getJSON(url[0] + '?callback=?', params, function(response) {
             $.each(response.result, function(i, result) {
                 var obj = {text: result['name']}.text;
-                var insertar = "<tr>" + "<td>" + obj + "</td>" + "</tr>";
+                var notable = {text: result['notable']['name']}.text;
+                var insertar = "<tr class='first'>"+ "<td>"+ (i+1) +"</td>" + "<td>" + obj + "</td>"+ "<td>"+notable+"</td>" + "</tr>";
                 $('.mostrar').append(insertar);
-			});/*--------------------each----------------------*/
-        	}); /*---------getjson-----------*/
-            }
-
-            else if($(this).prop("checked") == false){
-                $(".mostrar").empty();
-            }
-        });
+            });/*--each--*/
+        }); /*--getjson--*/
+        }
+        else if($(this).prop("checked") == false){
+            $(".first").empty();
+	    $('.search').show();
+        }
     });
+});
 
 /*------------------------Second filter---------------------------------*/
 $(document).ready(function(){
         $('input[name="people"]').click(function(){
+	    $('.search').hide();
             if($(this).prop("checked") == true){
-                var b = $('input:text[name=nombre]').val();
-        	var service_url = 'https://www.googleapis.com/freebase/v1/search';
         	var params = {
-            	'query': b,
-            	'filter': '(all type:/people/)',
+            	'query': busqueda[0],
+            	'filter': filters[1],
             	'limit': 5,
             	'indent': true
         	};
-        $.getJSON(service_url + '?callback=?', params, function(response) {
+        $.getJSON(url[0] + '?callback=?', params, function(response) {
             $.each(response.result, function(i, result) {
                 var obj = {text: result['name']}.text;
-                var insertar = "<tr>" + "<td>" + obj + "</td>" + "</tr>";
+                var notable = {text: result['notable']['name']}.text;
+                var insertar = "<tr class='second'>"+ "<td>"+ (i+1) +"</td>" + "<td>" + obj + "</td>"+ "<td>"+notable+"</td>" + "</tr>";
                 $('.mostrar').append(insertar);
-			});/*--------------------each----------------------*/
-        	}); /*---------getjson-----------*/
+			});/*--each--*/
+        	}); /*--getjson--*/
             }
 
             else if($(this).prop("checked") == false){
-                $(".mostrar").empty();
+                $(".second").empty();
+		$('.search').show();
             }
         });
     });
@@ -79,26 +94,27 @@ $(document).ready(function(){
 /*------------------------Third filter---------------------------------*/
 $(document).ready(function(){
         $('input[name="book"]').click(function(){
+	    $('.search').hide();
             if($(this).prop("checked") == true){
-                var b = $('input:text[name=nombre]').val();
-        	var service_url = 'https://www.googleapis.com/freebase/v1/search';
         	var params = {
-            	'query': b,
-            	'filter': '(all type:/book/)',
+            	'query': busqueda[0],
+            	'filter': filters[2],
             	'limit': 5,
             	'indent': true
         	};
-        $.getJSON(service_url + '?callback=?', params, function(response) {
+        $.getJSON(url[0] + '?callback=?', params, function(response) {
             $.each(response.result, function(i, result) {
                 var obj = {text: result['name']}.text;
-                var insertar = "<tr>" + "<td>" + obj + "</td>" + "</tr>";
+                var notable = {text: result['notable']['name']}.text;
+                var insertar = "<tr class='third'>"+ "<td>"+ (i+1) +"</td>" + "<td>" + obj + "</td>"+ "<td>"+notable+"</td>" + "</tr>";
                 $('.mostrar').append(insertar);
-			});/*--------------------each----------------------*/
-        	}); /*---------getjson-----------*/
+			});/*--each--*/
+        	}); /*--getjson--*/
             }
 
             else if($(this).prop("checked") == false){
-                $(".mostrar").empty();
+                $(".third").empty();
+		$('.search').show();
             }
         });
     });
