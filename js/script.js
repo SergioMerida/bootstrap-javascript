5d0f40c15d15e83548a1a03b7c47f7d7e64723c0
Sergio Merida <sergiotabarini@hotmail.com>
2014-12-03 20:13:30 -0600

    VF

--- js/script.js
+++ js/script.js
@@ -1,118 +1,65 @@
 var busqueda = "";
-$(document).ready(function(){
-    $('.d').hide();
-    $('.c').hide();
-    $('#butt').click(function(){
-    $(".search").empty();
-        busqueda = $('input:text[name=nombre]').val();
-        var service_url = 'https://www.googleapis.com/freebase/v1/search';
-        var params = {
-            'query': busqueda,
-            'limit': 50,
-            'indent': true
-    };
-        $.getJSON(service_url + '?callback=?', params, function(response) {
-            $.each(response.result, function(i, result) {
-                var obj = {text: result['name']}.text;
-                var notable = {text: result['notable']['name']}.text;                
-		var insertar = "<tr class='search'>"+ "<td>"+ (i+1) +"</td>" + "<td>" + obj + "</td>"+ "<td>"+notable+"</td>" + "</tr>";
-                $('.mostrar').append(insertar);
-            });/*--each--*/
-        }); /*--getjson--*/
-        $('.check').prop('checked', false);
-    }); /*--click--*/
-	
-    $(".clear").click(function() {
-        $(".search").empty();
-    }); /*--Clear--*/
+$('.d').hide();
+$('.c').hide();
+$("#butt").click(function( event ) {
+	$(".mostrar").empty();
+	busqueda = $("#nombre").val();
+	$("#nombre").val("");
+	var service_url = 'https://www.googleapis.com/freebase/v1/search';
+	var params = {
+		'query': busqueda,
+		'limit': 10,
+		'indent': true
+	};
 
-}); /*--document--*/
-
-
-/*------------------*/
-$(document).ready(function(){
-$('input[name="foo"]').click(function(){
-    $('.d').show();
-    $('.c').show();
-	$('.search').hide();
-        if($(this).prop("checked") == true){
-            var params = {
-        	'query': busqueda[0],
-		    'filter': filters[0],
-        	'limit': 5,
-        	'indent': true
-    		};
-	    $.getJSON(url[0] + '?callback=?', params, function(response) {
-            $.each(response.result, function(i, result) {
-                var obj = {text: result['name']}.text;
-                var notable = {text: result['notable']['name']}.text;
-                var insertar = "<tr class='first'>"+ "<td>"+ (i+1) +"</td>" + "<td>" + obj + "</td>"+ "<td>"+notable+"</td>" + "</tr>";
-                $('.mostrar').append(insertar);
-            });/*--each--*/
-        }); /*--getjson--*/
-        }
-        else if($(this).prop("checked") == false){
-            $(".first").empty();
-	    $('.search').show();
-            $('.d').hide();
-            $('.c').hide();
-        }
-    });
+	$.getJSON(service_url + '?callback=?', params, function(response) {
+		var numero = 0;
+		$.each(response.result, function(i, result) {
+			var obj = {text: result['name']}.text;
+			var notable = {text: result['notable']['name']}.text;
+			var insertar = "<tr class='search'>"+ "<td>"+ (i+1) +"</td>" + "<td>" + obj + "</td>"+ "<td>"+notable+"</td>" + "</tr>";
+			$('.mostrar').append(insertar);
+		});
+	});
 });
 
-/*------------------------Second filter---------------------------------*/
-$(document).ready(function(){
-        $('input[name="people"]').click(function(){
-	    $('.search').hide();
-            if($(this).prop("checked") == true){
-        	var params = {
-            	'query': busqueda[0],
-            	'filter': filters[1],
-            	'limit': 5,
-            	'indent': true
-        	};
-        $.getJSON(url[0] + '?callback=?', params, function(response) {
-            $.each(response.result, function(i, result) {
-                var obj = {text: result['name']}.text;
-                var notable = {text: result['notable']['name']}.text;
-                var insertar = "<tr class='second'>"+ "<td>"+ (i+1) +"</td>" + "<td>" + obj + "</td>"+ "<td>"+notable+"</td>" + "</tr>";
-                $('.mostrar').append(insertar);
-			});/*--each--*/
-        	}); /*--getjson--*/
-            }
 
-            else if($(this).prop("checked") == false){
-                $(".second").empty();
-		$('.search').show();
-            }
-        });
-    });
+$('input[name="filtros"]').change(function(){
+	$(".mostrar").empty();
+	var checkedEnable = [];
+	var filterUrls = [" type:/music/", " type:/people/", " type:/book/", " type:/music/artist", " type:/music/album"];
+	var filterString = "";
+	$("input[name=filtros]:checked").each(function(){
+		$('.d').show();
+		$('.c').show();
+		checkedEnable.push(this.value);
+	});
+	
+	for (var i=0;i<checkedEnable.length;i++){
+		filterString = filterString + filterUrls[checkedEnable[i]];
+	};
+	
+	filterString = "(all"+filterString+")";
+	var service_url = 'https://www.googleapis.com/freebase/v1/search';
+	var params = {
+		'query': busqueda,
+		'filter': filterString,
+		'limit': 10,
+		'indent': true
+	};
+	$.getJSON(service_url + '?callback=?', params, function(response) {
+		var numero = 0;
+		$.each(response.result, function(i, result) {
+			var obj = {text: result['name']}.text;
+			var notable = {text: result['notable']['name']}.text;
+			var insertar = "<tr class='search'>"+ "<td>"+ (i+1) +"</td>" + "<td>" + obj + "</td>"+ "<td>"+notable+"</td>" + "</tr>";
+			$('.mostrar').append(insertar);
+		});
+	});
+});
 
-/*------------------------Third filter---------------------------------*/
-$(document).ready(function(){
-        $('input[name="book"]').click(function(){
-	    $('.search').hide();
-            if($(this).prop("checked") == true){
-        	var params = {
-            	'query': busqueda[0],
-            	'filter': filters[2],
-            	'limit': 5,
-            	'indent': true
-        	};
-        $.getJSON(url[0] + '?callback=?', params, function(response) {
-            $.each(response.result, function(i, result) {
-                var obj = {text: result['name']}.text;
-                var notable = {text: result['notable']['name']}.text;
-                var insertar = "<tr class='third'>"+ "<td>"+ (i+1) +"</td>" + "<td>" + obj + "</td>"+ "<td>"+notable+"</td>" + "</tr>";
-                $('.mostrar').append(insertar);
-			});/*--each--*/
-        	}); /*--getjson--*/
-            }
 
-            else if($(this).prop("checked") == false){
-                $(".third").empty();
-		$('.search').show();
-            }
-        });
-    });
+$(".clear").click(function() {
+$(".search").empty();
+}); /*--Clear--*/
 
